898593126f15d2ccf39e5730cedb4dd2576f2ee8
Jérome Eertmans <jeertmans@icloud.com>
2022-06-01 17:10:52 +0200

    fix(lib): avoid panicking on string slices
    This fixes the problem that occurred when the string was not made of purely utf-8 chars. E.g.: `ltrs check --text "Un essai de texte en français à controler"`

--- src/lib/server.rs
+++ src/lib/server.rs
@@ -330,8 +330,9 @@ impl ServerClient {
                 footer: vec![],
                 slices: vec![Slice {
                     source: &m.context.text,
-                    line_start: 1 + m.sentence[..m.offset]
+                    line_start: 1 + m.sentence
                         .chars()
+                        .take(m.offset)
                         .filter(|c| *c == '\n')
                         .count(),
                     origin: None,
