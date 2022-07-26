b87b611162fe7f913566a863b6d27ab055664098
Jérome Eertmans <jeertmans@icloud.com>
2022-07-26 13:44:57 +0200

    wip(tests): more tests

--- src/lib/check.rs
+++ src/lib/check.rs
@@ -63,7 +63,41 @@ impl DataAnnotation {
     }
 }
 
-#[derive(Clone, Debug, Default, Deserialize, PartialEq, Eq)]
+
+#[cfg(test)]
+mod data_annotation_tests {
+
+    use crate::check::DataAnnotation;
+
+    #[test]
+    fn test_text() {
+        let da = DataAnnotation::new_text("Hello".to_string());
+
+        assert_eq!(da.text.unwrap(), "Hello".to_string());
+        assert!(da.markup.is_none());
+        assert!(da.interpret_as.is_none());
+    }
+
+    #[test]
+    fn test_markup() {
+        let da = DataAnnotation::new_markup("<a>Hello</a>`".to_string());
+
+        assert!(da.text.is_none());
+        assert_eq!(da.markup.unwrap(), "<a>Hello</a>".to_string());
+        assert!(da.interpret_as.is_none());
+    }
+
+    #[test]
+    fn test_interpreted_markup() {
+        let da = DataAnnotation::new_interpreted_markup("<a>Hello</a>`".to_string(), "Hello".to_string());
+
+        assert!(da.text.is_none());
+        assert_eq!(da.markup.unwrap(), "<a>Hello</a>".to_string());
+        assert_eq!(da.interpret_as.unwrap(), "Hello".to_string());
+    }
+}
+
+#[derive(Clone, Debug, Default, Deserialize, PartialEq)]
 #[non_exhaustive]
 /// Alternative text to be checked.
 pub struct Data {
@@ -151,11 +185,7 @@ pub struct CheckRequest {
     #[clap(short = 'r', long, takes_value = false)]
     /// If present, raw JSON output will be printed instead of annotated text.
     pub raw: bool,
-    #[cfg(feature = "cli")]
-    #[clap(short = 'm', long, takes_value = false)]
-    /// If present, more context (i.e., line number and line offset) will be added to response.
-    pub more_context: bool,
-    #[cfg_attr(feature = "cli", clap(short = 't', long, conflicts_with = "data",))]
+    #[cfg_attr(feature = "cli", clap(short = 't', long, conflicts_with = "data"))]
     /// The text to be checked. This or 'data' is required.
     pub text: Option<String>,
     #[cfg_attr(feature = "cli", clap(short = 'd', long, conflicts_with = "text"))]
@@ -290,7 +320,30 @@ impl CheckRequest {
     }
 }
 
-/// Responses
+
+#[cfg(test)]
+mod request_tests {
+
+    use crate::CheckRequest;
+
+    #[test]
+    fn test_with_text() {
+        let req = CheckRequest::default().with_text("hello".to_string());
+
+        assert_eq!(req.text.unwrap(), "hello".to_string());
+        assert!(req.data.is_none());
+    }
+
+    #[test]
+    fn test_with_data() {
+        let req = CheckRequest::default().with_text("hello".to_string());
+
+        assert_eq!(req.text.unwrap(), "hello".to_string());
+        assert!(req.data.is_none());
+    }
+}
+
+/// Reponses
 
 #[derive(Clone, Debug, PartialEq, Deserialize, Serialize)]
 #[non_exhaustive]
