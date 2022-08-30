470fac08ad7bbf537817f8db0475aaf4a9380d30
Jérome Eertmans <jeertmans@icloud.com>
2022-08-30 13:35:05 +0200

    fix(lint): please clippy

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -1,5 +1,9 @@
 #![deny(missing_docs)]
 #![deny(missing_debug_implementations)]
+#![warn(
+     clippy::must_use_candidate,
+)]
+#![allow(clippy::doc_markdown, clippy::module_name_repetitions)]
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]
 #![doc = include_str!("../../README.md")]
 //!
