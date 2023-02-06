2f4636fb9488341b9ab9fed262fc47d2fd91f058
Jérome Eertmans <jeertmans@icloud.com>
2023-02-06 15:19:18 +0100

    chore(docs): change header level

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -5,7 +5,7 @@
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]
 #![doc = include_str!("../../README.md")]
 //!
-//! # Note
+//! ## Note
 //!
 //! Most structures in this library are marked with
 //! ```ignore
