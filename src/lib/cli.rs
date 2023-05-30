7d5ec10f2acde2b3d3015e19fd2d288e30b4215b
Jérome Eertmans <jeertmans@icloud.com>
2023-05-30 10:49:47 +0200

    try(tests): printout the server client

--- src/lib/cli.rs
+++ src/lib/cli.rs
@@ -109,6 +109,8 @@ impl Cli {
 
         let server_client: ServerClient = self.server_cli.into();
 
+        println!("Server client: {:?}", server_client);
+
         match self.command {
             Command::Check(cmd) => {
                 let mut request = cmd.request;
