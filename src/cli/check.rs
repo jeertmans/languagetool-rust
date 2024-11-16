a90bd155b81799a15b4f0456704d64903aef1950
rolv <rolv.apneseth@gmail.com>
2024-11-16 12:54:33 +0000

    refactor: remove unnecessary code duplication from `cli/check`

--- src/cli/check.rs
+++ src/cli/check.rs
@@ -6,7 +6,7 @@
 //! - annotated data, if `--data TEXT` is provided;
 //! - text from file(s), if `[FILE(S)]...` are provided.
 //! - raw text through `stdin`, if nothing else is provided.
-use std::{borrow::Cow, io::Write, mem, path::PathBuf};
+use std::{borrow::Cow, io::Write, path::PathBuf};
 
 use clap::{Args, Parser, ValueEnum};
 use serde::{Deserialize, Serialize};
@@ -15,7 +15,7 @@ use termcolor::{StandardStream, WriteColor};
 use crate::{
     api::{
         check::{
-            parse_language_code, split_len, Data, DataAnnotation, Level, Request, DEFAULT_LANGUAGE,
+            self, parse_language_code, Data, DataAnnotation, Level, Request, DEFAULT_LANGUAGE,
         },
         server::ServerClient,
     },
@@ -83,7 +83,7 @@ pub enum FileType {
 impl ExecuteSubcommand for Command {
     /// Executes the `check` subcommand.
     async fn execute(self, mut stdout: StandardStream, server_client: ServerClient) -> Result<()> {
-        let mut request = self.request;
+        let mut request: check::Request = self.request.into();
         #[cfg(feature = "annotate")]
         let color = stdout.supports_color();
 
@@ -100,7 +100,7 @@ impl ExecuteSubcommand for Command {
 
             if request.text.is_none() {
                 // Handle annotated data
-                let response = server_client.check(&request.into()).await?;
+                let response = server_client.check(&request).await?;
                 writeln!(&mut stdout, "{}", serde_json::to_string_pretty(&response)?)?;
                 return Ok(());
             };
@@ -278,116 +278,6 @@ impl From<CliRequest> for Request<'_> {
     }
 }
 
-impl CliRequest {
-    /// Create a new empty request with language set to `"auto"`.
-    #[must_use]
-    pub fn new() -> Self {
-        Self {
-            language: "auto".to_string(),
-            ..Default::default()
-        }
-    }
-
-    /// Set the text to be checked and remove potential data field.
-    #[must_use]
-    pub fn with_text<T: Into<String>>(mut self, text: T) -> Self {
-        self.text = Some(text.into());
-        self.data = None;
-        self
-    }
-
-    /// Set the data to be checked and remove potential text field.
-    #[must_use]
-    pub fn with_data(mut self, data: CliData) -> Self {
-        self.data = Some(data);
-        self.text = None;
-        self
-    }
-
-    /// Set the data (obtained from string) to be checked and remove potential
-    /// text field
-    pub fn with_data_str(self, data: &str) -> serde_json::Result<Self> {
-        serde_json::from_str(data).map(|data| self.with_data(data))
-    }
-
-    /// Set the language of the text / data.
-    #[must_use]
-    pub fn with_language(mut self, language: String) -> Self {
-        self.language = language;
-        self
-    }
-
-    /// Return the text within the request.
-    ///
-    /// # Errors
-    ///
-    /// If both `self.text` and `self.data` are [`None`].
-    /// If any data annotation does not contain text or markup.
-    pub fn try_get_text(&self) -> Result<String> {
-        if let Some(ref text) = self.text {
-            Ok(text.clone())
-        } else if let Some(ref data) = self.data {
-            match data.annotation.len() {
-                0 => Ok(Default::default()),
-                1 => data.annotation[0].try_get_text(),
-                _ => {
-                    let mut text = String::new();
-
-                    for da in data.annotation.iter() {
-                        text.push_str(&da.try_get_text()?);
-                    }
-
-                    Ok(text)
-                },
-            }
-        } else {
-            Err(Error::InvalidRequest(
-                "missing either text or data field".to_string(),
-            ))
-        }
-    }
-
-    /// Return a copy of the text within the request.
-    /// Call [`Request::try_get_text`] but panic on error.
-    ///
-    /// # Panics
-    ///
-    /// If both `self.text` and `self.data` are [`None`].
-    /// If any data annotation does not contain text or markup.
-    #[must_use]
-    pub fn get_text(&self) -> String {
-        self.try_get_text().unwrap()
-    }
-
-    /// Split this request into multiple, using [`split_len`] function to split
-    /// text.
-    ///
-    /// # Errors
-    ///
-    /// If `self.text` is none.
-    pub fn try_split(mut self, n: usize, pat: &str) -> Result<Vec<Request>> {
-        let text = mem::take(&mut self.text)
-            .ok_or_else(|| Error::InvalidRequest("missing text field".to_string()))?;
-
-        Ok(split_len(&text, n, pat)
-            .iter()
-            .map(|text_fragment| self.clone().with_text(text_fragment.to_string()).into())
-            .collect())
-    }
-
-    /// Split this request into multiple, using [`split_len`] function to split
-    /// text.
-    /// Call [`Request::try_split`] but panic on error.
-    ///
-    /// # Panics
-    ///
-    /// If `self.text` is none.
-    #[must_use]
-    pub fn split(self, n: usize, pat: &str) -> Vec<Request> {
-        self.try_split(n, pat).unwrap()
-    }
-}
-
 /// Alternative text to be checked.
 #[derive(Clone, Debug, Default, Deserialize, PartialEq, Eq, Hash)]
 #[non_exhaustive]
@@ -438,70 +328,6 @@ impl From<CliDataAnnotation> for DataAnnotation<'_> {
     }
 }
 
-impl CliDataAnnotation {
-    /// Instantiate a new `CliDataAnnotation` with text only.
-    #[inline]
-    #[must_use]
-    pub fn new_text<T: Into<String>>(text: T) -> Self {
-        Self {
-            text: Some(text.into()),
-            markup: None,
-            interpret_as: None,
-        }
-    }
-
-    /// Instantiate a new `CliDataAnnotation` with markup only.
-    #[inline]
-    #[must_use]
-    pub fn new_markup<M: Into<String>>(markup: M) -> Self {
-        Self {
-            text: None,
-            markup: Some(markup.into()),
-            interpret_as: None,
-        }
-    }
-
-    /// Instantiate a new `CliDataAnnotation` with markup and its
-    /// interpretation.
-    #[inline]
-    #[must_use]
-    pub fn new_interpreted_markup<M: Into<String>, I: Into<String>>(
-        markup: M,
-        interpret_as: I,
-    ) -> Self {
-        Self {
-            interpret_as: Some(interpret_as.into()),
-            markup: Some(markup.into()),
-            text: None,
-        }
-    }
-
-    /// Return the text or markup within the data annotation.
-    ///
-    /// # Errors
-    ///
-    /// If this data annotation does not contain text or markup.
-    pub fn try_get_text(&self) -> Result<String> {
-        if let Some(ref text) = self.text {
-            Ok(text.clone())
-        } else if let Some(ref markup) = self.markup {
-            Ok(markup.clone())
-        } else {
-            Err(Error::InvalidDataAnnotation(format!(
-                "missing either text or markup field in {self:?}"
-            )))
-        }
-    }
-}
-
-impl<T: Into<CliDataAnnotation>> FromIterator<T> for CliData {
-    fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
-        let annotation = iter.into_iter().map(std::convert::Into::into).collect();
-        CliData { annotation }
-    }
-}
-
-#[cfg(feature = "cli")]
 impl std::str::FromStr for CliData {
     type Err = Error;
 
